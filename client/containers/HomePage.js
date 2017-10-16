586dbb251f3139560bebfe8b7b258a5cd0a4bffb
Syrotenko Igor <neloreck@gmail.com>
2017-10-16 12:46:05 +0300

    Added non-redux example of socket.io usage.

--- client/containers/HomePage.js
+++ client/containers/HomePage.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import socketIOClient from "socket.io-client";
+
 import {connect} from 'react-redux'
 import { bindActionCreators } from 'redux';
 
@@ -6,10 +8,29 @@ import testActions from '../actions/test'
 
 class HomePage extends Component
 {
-	constructor(props)
-	{
-		super(props);
-	}
+  constructor(props)
+  {
+    super(props);
+
+    this.state =
+      {
+        response: false,
+        inputText: "",
+
+        socketOptions:
+          {
+            endpoint: "http://127.0.0.1:3000",
+            socket: null
+          }
+      };
+
+    this.bindSocket = this.bindSocket.bind(this);
+  }
+
+  componentWillMount()
+  {
+    this.bindSocket();
+  }
 
 	componentDidMount()
 	{
@@ -17,24 +38,60 @@ class HomePage extends Component
 		this.props.testActions.testActionAsync();
 	}
 
-	render()
-	{
-		return (
-			<div>
-				<h1>Home Page</h1>
-			</div>
-		)
-	}
+	//=============================================================================
+
+  sendSocketMessage(e)
+  {
+    this.state.socket.emit('sendMessage', this.state.inputText)
+  }
+
+  handleInputChange(e)
+  {
+    this.setState({inputText:e.target.value});
+  }
+
+  handleInputDataRenew(data)
+  {
+    console.log(`Received update from the server: ${data}`);
+    this.setState({inputText: data});
+  }
+
+	bindSocket()
+  {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+
+    socket.on("renewMessageText", data => this.handleInputDataRenew(data));
+
+    this.setState({socket:socket}, console.log("Socket successfully bound."));
+  }
+
+	//==============================================================================
+
+  render()
+  {
+    return (
+     <div>
+       <input value={this.state.inputText}
+              onChange={ (e) => { this.handleInputChange(e) } } />
+       <br/>
+       <button onClick={ (e) => { this.sendSocketMessage(e) } }>
+         Send
+       </button>
+     </div>
+    );
+  }
 }
 
-
-function mapStateToProps (state) {
+function mapStateToProps (state)
+{
 	return {
 		test: state.get('test').toJS()
 	}
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch)
+{
 	return {
 		testActions: bindActionCreators(testActions, dispatch)
 	}
